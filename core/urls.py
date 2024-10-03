c2053faf14f00429b46eaf32fb2823d828a63641
WalterMelon~ <vgberny@gmail.com~>
2024-10-03 14:27:16 -0600

    para despliegue

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('store/', views.store, name="store"),
+    path('admin/', admin.site.urls),
 ]
